6b522b092579ecc1772602def61afbc175895938
Perkles <denise_lds@hotmail.com>
2022-07-08 10:27:10 -0300

    Operadores

---
+++ aulas/sintaxe-javascript/operadores.js
@@ -0,0 +1,49 @@
+//ATRIBUIÇÃO
+
+/*
+'=' atribui um valor à uma variável.
+'+=' atribui somando um valor à uma variável.
+'*=' atribui multiplicando um valor à uma variável.
+'/=' atribui dividindo um valor à uma variável.
+'%=' atribui adicionando o resto de uma divisao à uma variável.
+*/
+
+//ARITMÉTICOS
+
+/*
++
+-
+*
+** - potenciação
+/
+%
+++ - incrementa 1 ao valor
+'--' - decrementa 1 ao valor
+*/
+
+//COMPARAÇÃO
+
+/*
+'==' compara se dois valores são iguais
+'===' compara se os valores de determinadas variáveis são iguais e se são do mesmo tipo (numero, string...)
+'!=' compara se valores são diferentes
+'!==' compara se valores são diferentes e se sao de tipos diferentes
+'>' maior que
+'<' menor que
+'>=' maior ou igual
+'<=' menor ou igual
+*/
+
+//LÓGICOS
+
+/*
+'&&' E
+'||' OU
+'!' NÃO
+*/
+
+//CONDICIONAL (ternário)
+
+// '?' se for verdade ':' se não for (equivale a escrever um if e else)
+
+
